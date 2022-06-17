79a784abbefc9c8cb65af2c1c7fcddc70e6ac840
Massimo Luraschi <massimo.lura@gmail.com>
2022-06-17 08:32:57 +0200

    Got the project working

--- src/processor.ts
+++ src/processor.ts
@@ -1,6 +1,8 @@
 import * as ss58 from "@subsquid/ss58";
-import { SubstrateProcessor, toHex } from "@subsquid/substrate-processor";
-import { lookupArchive } from "@subsquid/archive-registry";
+import {
+  EventHandlerContext,
+  SubstrateProcessor,
+} from "@subsquid/substrate-processor";
 import { TypeormDatabase, EntityClass, Store } from "@subsquid/typeorm-store";
 import { Account, HistoricalBalance } from "./model";
 import { BalancesTransferEvent } from "./types/events";
@@ -12,64 +14,57 @@ const processor = new SubstrateProcessor(
 
 processor.setBatchSize(500);
 processor.setDataSource({
-  archive: lookupArchive("kusama")[0].url,
+  archive: "https://kusama.archive.subsquid.io/graphql",
   chain: "wss://kusama-rpc.polkadot.io",
 });
 
-const logEvery = process.env.SECRET_LOG_EVERY
-  ? Number(process.env.SECRET_LOG_EVERY)
-  : 30000;
-if (logEvery > 0) {
-  setInterval(() => {
-    console.log({ message: "test", param1: "test", ts: new Date() });
-  }, logEvery);
-} else {
-  console.log({ message: "log every disabled", level: "warn" });
+processor.addEventHandler("Balances.Transfer", processTransfers);
+
+async function processTransfers(
+  ctx: EventHandlerContext<Store, { event: { args: true } }>
+) {
+  const transfer = getTransferEvent(ctx);
+  const timestamp = BigInt(new Date(ctx.block.timestamp).valueOf());
+
+  // ss58.codec("kusama").encode(transfer.from)
+  const fromAcc = await getOrCreate(
+    ctx.store,
+    Account,
+    ss58.codec("kusama").encode(transfer.from)
+  );
+  fromAcc.wallet = fromAcc.id;
+  fromAcc.balance = fromAcc.balance || 0n;
+  fromAcc.balance -= transfer.amount;
+  await ctx.store.save(fromAcc);
+
+  const toAcc = await getOrCreate(
+    ctx.store,
+    Account,
+    ss58.codec("kusama").encode(transfer.to)
+  );
+  toAcc.wallet = toAcc.id;
+  toAcc.balance = toAcc.balance || 0n;
+  toAcc.balance += transfer.amount;
+  await ctx.store.save(toAcc);
+
+  await ctx.store.insert(
+    new HistoricalBalance({
+      id: `${ctx.event.id}-to`,
+      account: fromAcc,
+      balance: fromAcc.balance,
+      timestamp,
+    })
+  );
+
+  await ctx.store.insert(
+    new HistoricalBalance({
+      id: `${ctx.event.id}-from`,
+      account: toAcc,
+      balance: toAcc.balance,
+      timestamp,
+    })
+  );
 }
-
-processor.addEventHandler(
-  "Balances.Transfer",
-  {
-    data: {
-      event: { args: true },
-    },
-  } as const,
-  async (ctx) => {
-    const transfer = getTransferEvent(ctx);
-    const timestamp = BigInt(new Date(ctx.block.timestamp).valueOf());
-
-    // ss58.codec("kusama").encode(transfer.from)
-    const fromAcc = await getOrCreate(ctx.store, Account, toHex(transfer.from));
-    fromAcc.wallet = fromAcc.id;
-    fromAcc.balance = fromAcc.balance || 0n;
-    fromAcc.balance -= transfer.amount;
-    await ctx.store.save(fromAcc);
-
-    const toAcc = await getOrCreate(ctx.store, Account, toHex(transfer.to));
-    toAcc.wallet = toAcc.id;
-    toAcc.balance = toAcc.balance || 0n;
-    toAcc.balance += transfer.amount;
-    await ctx.store.save(toAcc);
-
-    await ctx.store.insert(
-      new HistoricalBalance({
-        id: `${ctx.event.id}-to`,
-        account: fromAcc,
-        balance: fromAcc.balance,
-        timestamp,
-      })
-    );
-
-    await ctx.store.insert(
-      new HistoricalBalance({
-        id: `${ctx.event.id}-from`,
-        account: toAcc,
-        balance: toAcc.balance,
-        timestamp,
-      })
-    );
-  }
-);
 
 processor.run();
 
